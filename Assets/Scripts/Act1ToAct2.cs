d2f757b0366f1b19a3e5ef5e21e6472c1088dce9
OGURE4I4EK <vladbelkov1428@gmail.com>
2026-01-11 13:58:21 +0700

    A bit patch

--- Assets/Scripts/Act1ToAct2.cs
+++ Assets/Scripts/Act1ToAct2.cs
@@ -5,13 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Act1ToAct2 : MonoBehaviour
 {
-    [SerializeField] GameObject canvas;
-    [SerializeField] GameObject canvas2;
-    [SerializeField] GameObject a;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<CharacterController>() != null)
-            canvas.SetActive(false);
-            canvas2.SetActive(true);
+        SceneManager.LoadScene("Act2New");
     }
 }
